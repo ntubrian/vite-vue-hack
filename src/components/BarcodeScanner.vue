ca6e25365a7f4675075efe2b822a2b5ff994c7c2
Brian Tzu-Pin Ho <k333hoisi@gmail.com>
2024-09-07 20:11:03 +0800

    fix: fix the camera stuck the web after scan

--- src/components/BarcodeScanner.vue
+++ src/components/BarcodeScanner.vue
@@ -5,15 +5,15 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue'
 import { useHtml5QrCode } from '../hooks/html5-qr-code'
-import { toast } from 'vue3-toastify'; // 引用
-import 'vue3-toastify/dist/index.css'; // 官方
+import { toast } from 'vue3-toastify'; 
+import 'vue3-toastify/dist/index.css'; 
 
 const html5QrCode = ref(null)
-const isProcessing = ref(false) 
+const isProcessing = ref(false) // 添加标志
 
 onMounted(() => {
   html5QrCode.value = useHtml5QrCode('barcode-reader')
-  html5QrCode.value.start(qrCodeSuccessCallback, qrCodeErrorCallback)
+  startScanner()
 })
 
 onUnmounted(() => {
@@ -21,6 +21,10 @@ onUnmounted(() => {
     html5QrCode.value.handleStop()
   }
 })
+
+const startScanner = () => {
+  html5QrCode.value.start(qrCodeSuccessCallback, qrCodeErrorCallback)
+}
 
 const qrCodeSuccessCallback = (decodedText) => {
   if (!isProcessing.value) {
@@ -40,7 +44,15 @@ const notify = (decodedText) => {
   } else {
     toast.error(`取消造訪景點${decodedText}`)
   }
-  isProcessing.value = false // 重置标志
+  isProcessing.value = false // 重置 mark
+  restartScanner() // 重新啟動
+}
+
+const restartScanner = () => {
+  html5QrCode.value.handleStop()
+  setTimeout(() => {
+    startScanner()
+  }, 1000) // 延遲1秒重新啟動掃描器
 }
 </script>
 
